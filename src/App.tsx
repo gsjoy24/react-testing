a318245d0ec1e3291ac9354cb5b63b393f63874b
gsjoy24 <gour.joy24@gmail.com>
2024-06-25 18:03:40 +0600

    added  first test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
 	return (
 		<>
 			<h1>Testing React</h1>
-	
 		</>
 	);
 }
--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,2 +1,8 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
+it('should render Testing React correctly', () => {
+	render(<App />);
+	const element = screen.getByText('Testing React');
+	expect(element).toBeInTheDocument();
+});
